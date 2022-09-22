8dd7fcf505838524ae351bcd4b5d3867b80dc7d8
Sushant Bhatia <sbhatia@sprinklr.com>
2022-09-22 22:06:31 +0530

    chore: Use spdSdk

--- templates/sprinklr-app/pages/hello-world/HelloWorld.tsx
+++ templates/sprinklr-app/pages/hello-world/HelloWorld.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next';
+import SprClient from '@sprinklrjs/app-sdk';
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 
 const HelloWorld: NextPage = () => {
+  const [sdkClient, setSdkClient] = useState<SprClient>();
+
+  useEffect(() => {
+    const initSdk = async () => {
+      const sprClient = new SprClient();
+      await sprClient.init();
+      setSdkClient(sdkClient);
+    };
+    initSdk();
+  }, []);
+
   return (
     <>
       <Head>
         <title>Hello World</title>
       </Head>
       <main>
-        <div id="app">Hello World</div>
+        <div id="app">{sdkClient ? 'Hello World' : 'Loading...'}</div>
       </main>
     </>
   );
