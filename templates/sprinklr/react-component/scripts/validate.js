c6424943307b85e7888bb3e4e55801f2f054585f
Abhishek Chawla <abhishek.chawla@sprinklr.com>
2023-12-05 15:17:33 +0530

    feat: code refactored

--- templates/sprinklr/react-component/scripts/validate.js
+++ templates/sprinklr/react-component/scripts/validate.js
@@ -1,33 +1,39 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
+const errors = [];
+
+function errorMessage(field, type){
+  return `Value of field '${field}' should be a non-empty ${type}`;
+}
+
 function validateWidget(config) {
   const requiredFields = ['id', 'title', 'url', 'scopes'];
   const optionalFields = ['props'];
 
   Object.entries(config).forEach(([key]) => {
     if (!requiredFields.includes(key) && !optionalFields.includes(key)) {
-      throw new Error(`field '${key}' is not allowed in widget config`);
+      errors.push(`Field '${key}' is not allowed in widget config`);
     }
   });
 
   requiredFields.forEach(field => {
     if (!(field in config)) {
-      throw new Error(`field '${field}' is required in widget config`);
+      errors.push(`Field '${field}' is required in widget config`);
     }
 
     if (field === 'scopes') {
       if (!(typeof config[field] === 'object' && config[field].length)) {
-        throw new Error(`value of field '${field}' should be a non-empty array`);
+        errors.push(errorMessage(field, 'array'));
       }
     } else if (!(typeof config[field] === 'string' && config[field])) {
-      throw new Error(`value of field '${field}' should be a non-empty string`);
+      errors.push(errorMessage(field, 'string'));
     }
   });
 
   optionalFields.forEach(field => {
-    if (config[field] && !(typeof config[field] === 'object')) {
-      throw new Error(`value of field '${field}' should be an object`);
+    if (config[field] && !(typeof config[field] === 'object' && Object.keys(config[field]).length)) {
+      errors.push(errorMessage(field, 'object'));
     }
   });
 }
@@ -43,28 +49,35 @@ function validateWidget(config) {
   try {
     Object.entries(config).forEach(([key]) => {
       if (!requiredFields.includes(key) && !optionalFields.includes(key)) {
-        throw new Error(`field '${key}' is not allowed in manifest.json`);
+        errors.push(`Field '${key}' is not allowed in manifest.json`);
       }
     });
 
     requiredFields.forEach(field => {
       if (!(field in config)) {
-        throw new Error(`field '${field}' is required in manifest.json`);
+        errors.push(`Field '${field}' is required in manifest.json`);
       }
 
       if (field === 'widgets') {
         if (!(typeof config[field] === 'object' && config[field].length)) {
-          throw new Error(`value of field '${field}' should be a non-empty array`);
+          errors.push(errorMessage(field, 'array'));
         }
         config.widgets.forEach(widget => {
           validateWidget(widget);
         });
       } else if (!(typeof config[field] === 'string' && config[field])) {
-        throw new Error(`value of field '${field}' should be a non-empty string`);
+        errors.push(errorMessage(field, 'string'));
       }
     });
   } catch (error) {
     console.log(error);
+    process.exit(1);
+  }
+
+  if(errors.length){
+    errors.forEach(error => {
+      console.log(`Validation Error: ${error}`);
+    })
     process.exit(1);
   }
 })();
