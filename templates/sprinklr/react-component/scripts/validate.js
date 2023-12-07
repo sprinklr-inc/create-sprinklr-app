954bc37bc6d50b1f1162f505bd375227654738d3
Abhishek Chawla <abhishek.chawla@sprinklr.com>
2023-12-07 14:12:34 +0530

    feat: code refactored

--- templates/sprinklr/react-component/scripts/validate.js
+++ templates/sprinklr/react-component/scripts/validate.js
@@ -1,83 +1,75 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
-function errorMessage(field, type) {
-  return `Value of field '${field}' should be a non-empty ${type}`;
-}
+const errorMessage = (field, type) => `Value of field '${field}' should be a non-empty ${type}`;
 
-function checkRedundantFields({ config, requiredFields, optionalFields }) {
-  const errors = [];
-
-  Object.entries(config).forEach(([key]) => {
+const checkRedundantFields = ({ config, requiredFields, optionalFields }) =>
+  Object.entries(config).reduce((acc, [key]) => {
     if (!requiredFields.includes(key) && !optionalFields.includes(key)) {
-      errors.push(`Field '${key}' is not allowed`);
+      acc.push(`Field '${key}' is not allowed`);
     }
-  });
-  return errors;
-}
+    return acc;
+  }, []);
 
-function validateRequiredFields({ config, requiredFields, isWidget }) {
-  const errors = [];
-
-  requiredFields.forEach(field => {
+const validateRequiredFields = ({ config, requiredFields, isWidget }) =>
+  requiredFields.reduce((acc, field) => {
     if (!(field in config)) {
-      errors.push(`Field '${field}' is required`);
+      acc.push(`Field '${field}' is required`);
     }
 
     const iterableField = isWidget ? 'scopes' : 'widgets';
     if (field === iterableField) {
       if (!(typeof config[field] === 'object' && config[field].length)) {
-        errors.push(errorMessage(field, 'array'));
+        acc.push(errorMessage(field, 'array'));
       }
     } else if (!(typeof config[field] === 'string' && config[field])) {
-      errors.push(errorMessage(field, 'string'));
+      acc.push(errorMessage(field, 'string'));
     }
-  });
-  return errors;
-}
 
-function validateWidget(config) {
+    return acc;
+  }, []);
+
+const validateWidget = config => {
   const requiredFields = ['id', 'title', 'url', 'scopes'];
   const optionalFields = ['props'];
 
-  const errors = [];
+  return [
+    ...checkRedundantFields({ config, requiredFields, optionalFields }),
+    ...validateRequiredFields({ config, requiredFields, isWidget: true }),
+    ...optionalFields.reduce((acc, field) => {
+      if (config[field] && !(typeof config[field] === 'object' && Object.keys(config[field]).length)) {
+        acc.push(errorMessage(field, 'object'));
+      }
+      return acc;
+    }, []),
+  ];
+};
 
-  errors.push(...checkRedundantFields({ config, requiredFields, optionalFields }));
-  errors.push(...validateRequiredFields({ config, requiredFields, isWidget: true }));
-
-  optionalFields.forEach(field => {
-    if (config[field] && !(typeof config[field] === 'object' && Object.keys(config[field]).length)) {
-      errors.push(errorMessage(field, 'object'));
-    }
-  });
-  return errors;
-}
-
-(function isManifestValid() {
+const isManifestValid = () => {
   const requiredFields = ['name', 'version', 'widgets', 'integrationType'];
   const optionalFields = ['basePath'];
-  const errors = [];
 
   console.log('Validating manifest.json...');
   const path = resolve(process.cwd(), 'manifest.json');
   const config = JSON.parse(readFileSync(path));
 
-  try {
-    errors.push(...checkRedundantFields({ config, requiredFields, optionalFields }));
-    errors.push(...validateRequiredFields({ config, requiredFields, isWidget: false }));
-
-    config.widgets?.forEach(widget => {
-      errors.push(...validateWidget(widget));
-    });
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+  const errors = [
+    ...checkRedundantFields({ config, requiredFields, optionalFields }),
+    ...validateRequiredFields({ config, requiredFields, isWidget: false }),
+    ...(config.widgets || []).reduce((acc, widget) => {
+      acc.push(...validateWidget(widget));
+      return acc;
+    }, []),
+  ];
 
   if (errors.length) {
     errors.forEach(error => {
       console.log(`Validation Error: ${error}`);
     });
     process.exit(1);
+  } else {
+    console.log(`Successfully validated!`);
   }
-})();
+};
+
+isManifestValid();
